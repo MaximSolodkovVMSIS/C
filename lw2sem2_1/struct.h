e802c59554a2ed317d847759c4e4355193a1e6da
MaximSolodkovVMSIS <112771644+MaximSolodkovVMSIS@users.noreply.github.com>
2023-03-15 12:56:27 +0300

    LW2sem2

---
+++ lw2sem2_1/struct.h
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+
+typedef struct Words{
+	char* word;
+	int length;
+} Words;
+
+typedef struct Dictionsry{
+	char* word1;
+	char* word2;
+} Dictionary;
