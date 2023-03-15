d7cc4c53b7dff7d3ed7f6553e3709c2b7b3e1c03
MaximSolodkovVMSIS <112771644+MaximSolodkovVMSIS@users.noreply.github.com>
2023-03-15 13:14:07 +0300

    LW2sem2

---
+++ LW2sem2/lw2sem2_1/main.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+#include"function.h"
+
+int main() {
+	Words* array = NULL;
+	Dictionary* arr = NULL;
+	int size_of_words = 0;
+	int size_of_dictionary = 0;
+	words_for_dictionary(&array, &arr, &size_of_words, &size_of_dictionary);
+	input_dictionary(&arr, &size_of_dictionary);
+	zipper();
+	print_file_size("C:\\programming\\C\\zipper\\arhfile.txt");
+	free(array);
+	free(arr);
+	return 0;
+}
