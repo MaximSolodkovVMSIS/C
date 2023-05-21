737b263ebaf2e5440a949acd2c1b2d21a0ad6b75
MaximSolodkovVMSIS <112771644+MaximSolodkovVMSIS@users.noreply.github.com>
2023-05-21 15:40:29 +0300

    Update build.bat

--- build.bat
+++ build.bat
@@ -1,2 +1,5 @@
-msbuild LW1sem2/LW1sem2.vcxproj
-
+msbuild LW1sem2\LW1sem2.vcxproj
+msbuild LW2sem2\lw2sem2_1\lw2sem2_1.vcxproj
+msbuild LW3sem2\LW3sem2.vcxproj
+msbuild LW4Sem2\LW4Sem2.vcxproj
+msbuild LW5sem2\LW5sem2.vcxproj
